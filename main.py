3c196fbe78c499194ab2c506d0483f1bb48195c2
woomin0225 <rkddnals0225@gmail.com>
2025-04-23 23:53:39 +0900

    while문 추가 및 오류

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #계산기 프로그램 만들기
 # 두 수를 입력받아 사칙연산을 수행하는 계산기 프로그램
 # 사칙연산을 수행하는 함수 정의
+
+
 def plus(a, b):
     return a + b
 
@@ -16,29 +18,56 @@ def divi(a, b):
         return "0으로 나눌수 없습니다."
     return a / b
 
+# while 조건문 만들기
+
+while True:
+
 # 사용자로부터 두 수와 연산자를 입력받기
-num1 = int(input ("첫 번째 숫자를 입력하세요"))
-num2 = int(input ("두 번째 숫자를 입력하세요"))
-simbol = input("어떤 연산을 할지 선택하세요 +, -, *, /): ")
+    try:
+        num1 = int(input ("첫 번째 숫자를 입력하세요"))
+        num2 = int(input ("두 번째 숫자를 입력하세요"))
+        break
 
-if simbol == "+":
+        #오류방지
+    except ValueError:
+        print("숫자만 입력해주세요!")
+        
+
+        # 연산자 잘못 입력시 되돌아가는 범위 축소
+        # 실행시 num을 묻지않고 연산으로 넘어가는 문제 발견
+    while True:
+        simbol = input("어떤 연산을 할지 선택하세요 +, -, *, /): ")
+
     
-    print(plus(num1, num2))
+        if simbol == "+":
+            print(plus(num1, num2))
+            break
+        elif simbol == "-":
+            print(minus(num1, num2))
+            break
 
-elif simbol == "-":
-    
-    print(minus(num1, num2))
+        elif simbol == "*":
+            print( multi(num1, num2))
+            break
 
-elif simbol == "*":
-   
-    print( multi(num1, num2))
+        elif simbol == "/":
+            print( divi(num1, num2))
+            break
 
-elif simbol == "/":
-   
-    print( divi(num1, num2))
+        else:
+            print("정확한 기호를 입력하세요.")
+        
 
-else:
-    print("정확한 기호를 입력하세요.")
+        
 
+# 계산 완료 후 재실행 유무 묻기
 
-
+    again = input("다시 계산하시겠습니까? (y/n)")
+    if again.lower() == "y":
+            continue
+    elif again.lower() == "n" :
+            print("프로그램을 종료합니다")
+            break
+    else:
+            print("정확한 값을 입력해주세요")
+        
