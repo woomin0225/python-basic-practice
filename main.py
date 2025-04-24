19366fd5bf40a95857d866dd3c3b504c849fc0a0
woomin0225 <rkddnals0225@gmail.com>
2025-04-24 23:11:49 +0900

    반복문

--- main.py
+++ main.py
@@ -24,50 +24,53 @@ while True:
 
 # 사용자로부터 두 수와 연산자를 입력받기
     try:
-        num1 = int(input ("첫 번째 숫자를 입력하세요"))
-        num2 = int(input ("두 번째 숫자를 입력하세요"))
-        break
-
-        #오류방지
-    except ValueError:
-        print("숫자만 입력해주세요!")
-        
+        num1 = int(input ("첫 번째 숫자를 입력하세요 = "))
+        num2 = int(input ("두 번째 숫자를 입력하세요 = "))
+    
 
         # 연산자 잘못 입력시 되돌아가는 범위 축소
         # 실행시 num을 묻지않고 연산으로 넘어가는 문제 발견
-    while True:
-        simbol = input("어떤 연산을 할지 선택하세요 +, -, *, /): ")
+        while True:
+            simbol = input("어떤 연산을 할지 선택하세요 +, -, *, /): ")
 
-    
-        if simbol == "+":
-            print(plus(num1, num2))
-            break
-        elif simbol == "-":
-            print(minus(num1, num2))
-            break
+            if simbol == "+":
+                print(plus(num1, num2))
+                break
+            elif simbol == "-":
+                print(minus(num1, num2))
+                break
 
-        elif simbol == "*":
-            print( multi(num1, num2))
-            break
+            elif simbol == "*":
+                print( multi(num1, num2))
+                break
 
-        elif simbol == "/":
-            print( divi(num1, num2))
-            break
+            elif simbol == "/":
+                print( divi(num1, num2))
+                break
 
-        else:
-            print("정확한 기호를 입력하세요.")
-        
-
+            else:
+                print("정확한 기호를 입력하세요!")
+                
+    except ValueError:
+        print("숫자만 입력하세요!")    
+        continue
         
 
 # 계산 완료 후 재실행 유무 묻기
-
-    again = input("다시 계산하시겠습니까? (y/n)")
-    if again.lower() == "y":
-            continue
-    elif again.lower() == "n" :
-            print("프로그램을 종료합니다")
-            break
-    else:
-            print("정확한 값을 입력해주세요")
+    while True:
         
+        try:
+            again = input("다시 계산하시겠습니까? (y/n)")
+
+            if again.lower() == "y":
+                break
+
+            elif again.lower() == "n" :
+                print("프로그램을 종료합니다")
+                exit()
+
+            else:
+                print("정확한 값을 입력해주세요!")
+
+        except ValueError:
+            print("정확한 값을 입력해주세요!")
